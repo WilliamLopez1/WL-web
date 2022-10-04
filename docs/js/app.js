8aaf1d00cce02b9050933ab0662a4af212898244
WilliamLopez1 <william.lopez.leon@gmail.com>
2022-10-04 00:15:22 -0500

    bubbles

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,21 +2,29 @@ window.addEventListener('load', function(){
     new Glider(document.querySelector('.carousel__lista'),{
         slidesToShow: 1,
         slidesToScroll: 1,
-        draggable: true,
         dots: '.carousel__indicadores',
+        draggable: true,
         arrows: 
         {
         prev: '.carousel__anterior',
         next: '.carousel__siguiente'
         },
+        dots: '.carousel__indicadores',
         responsive: [
             {
               // screens greater than >= 775px
               breakpoint: 768,
               settings: {
                 // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '2',
-                slidesToScroll: '2',
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: '.carousel__indicadores',
+                draggable: true,
+                arrows: 
+                {
+                prev: '.carousel__anterior',
+                next: '.carousel__siguiente'
+                }
               }
             },{
               // screens greater than >= 1024px
@@ -24,6 +32,13 @@ window.addEventListener('load', function(){
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
+                dots: '.carousel__indicadores',
+                draggable: true,
+                arrows: 
+                {
+                prev: '.carousel__anterior',
+                next: '.carousel__siguiente'
+                }
               }
             }
           ]
